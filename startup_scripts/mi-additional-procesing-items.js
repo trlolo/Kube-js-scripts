93e58761316552e0e4ce5b9e188527d5e02a90e2
trlolo <43910955+trlolo@users.noreply.github.com>
2022-01-25 14:59:45 +0300

    Add files via upload

---
+++ startup_scripts/mi-additional-procesing-items.js
@@ -0,0 +1,7 @@
+onEvent('item.registry', event => {
+  event.create('impure_uranium_sulfate', item => item.displayName('Impure Uranium Sulfate'))
+  event.create('lead_sulfate', item => item.displayName('Lead Sulfate'))
+  event.create('lead_sulfide', item => item.displayName('Lead Sulfide'))
+  event.create('pure_uranium_sulfate', item => item.displayName('Pure Uranium Sulfate'))
+  event.create('uranium_trioxide', item => item.displayName('Uranium Trioxide'))
+})
