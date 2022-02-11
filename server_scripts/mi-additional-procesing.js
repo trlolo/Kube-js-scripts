0640384b60668e5cdaa48b7fc87b0db68c7629f6
trlolo <43910955+trlolo@users.noreply.github.com>
2022-02-11 22:52:20 +0300

    Add files via upload

--- server_scripts/mi-additional-procesing.js
+++ server_scripts/mi-additional-procesing.js
@@ -131,58 +131,6 @@ onEvent('recipes', (event) => {
 ]
 })
  event.custom({
-  "id":"inert_cell_production",
-  "type": "modern_industrialization:assembler",
-  "eu": 8,
-  "duration": 300,
-  "item_inputs": [
-  {
-      "item": "modern_industrialization:gold_plate",
-      "amount": 4
-  },
-  {
-      "tag": "c:glass_panes",
-      "amount": 1
-  }
-],
-  "fluid_inputs": {
-      "fluid": "modern_industrialization:soldering_alloy",
-      "amount": 250
-  },
-  "item_outputs": {
-      "item": "kubejs:inert_cell",
-      "amount": 1
-  }
-})
- event.custom({
-  "id":"nitric_acid_production",
-  "type": "modern_industrialization:chemical_reactor",
-  "eu": 32,
-  "duration": 3000,
-  "item_inputs": {
-      "item": "kubejs:inert_cell",
-      "amount": 6
-    },
-  "fluid_inputs": [
-    {
-      "fluid": "modern_industrialization:nitrogen",
-      "amount": 3000
-    },
-    {
-      "fluid": "modern_industrialization:oxygen",
-      "amount": 7500
-    },
-    {
-      "fluid": "minecraft:water",
-      "amount": 3000
-    }
-  ],
-  "item_outputs": {
-      "item": "kubejs:nitric_acid_cell",
-      "amount": 6
-  }
-})
- event.custom({
   "id":"nitric_copper_disolition",
   "type": "modern_industrialization:chemical_reactor",
   "eu": 12,
@@ -488,7 +436,7 @@ onEvent('recipes', (event) => {
   {
       "item": "kubejs:pure_iridium_chloride",
       "amount": 1,
-	  "probability" : 0.1
+	  "probability" : 0.3
   }
 ]
 })
@@ -844,10 +792,16 @@ onEvent('recipes', (event) => {
       "item": "kubejs:titanium_chloride_cell",
       "amount": 1
   },
-  "item_outputs": {
+  "item_outputs": [
+  {
       "item": "modern_industrialization:titanium_dust",
       "amount": 1
   },
+  {
+      "item": "kubejs:inert_cell",
+      "amount": 1
+  }
+],
   "fluid_outputs" : {
     "fluid" : "modern_industrialization:chlorine",
     "amount" : 2000
@@ -1014,6 +968,69 @@ onEvent('recipes', (event) => {
       "item": "kubejs:gold_chloride",
       "amount": 1,
 	  "probability" : 0.5
+  }
+]
+})
+ event.custom({
+  "id":"salt_water_disolition",
+  "type": "modern_industrialization:chemical_reactor",
+  "eu": 8,
+  "duration": 500,
+  "item_inputs": [
+    {
+      "item": "modern_industrialization:salt_crushed_dust",
+      "amount": 1
+    },
+    {
+      "item": "kubejs:heatproof_crucible",
+      "amount": 1
+    }
+],
+  "fluid_inputs": {
+      "fluid": "minecraft:water",
+      "amount": 6000
+    },
+  "item_outputs": {
+      "item": "kubejs:cold_brine_crucible",
+      "amount": 1
+  }
+})
+ event.custom({
+  "id":"brine_crucible_heating",
+  "type": "modern_industrialization:blast_furnace",
+  "eu": 32,
+  "duration": 1200,
+  "item_inputs": {
+      "item": "kubejs:cold_brine_crucible",
+      "amount": 1
+    },
+  "item_outputs": {
+      "item": "kubejs:hot_salt_crucible",
+      "amount": 1
+    },
+})
+ event.custom({
+  "id":"salt_purification",
+  "type": "modern_industrialization:centrifuge",
+  "eu": 8,
+  "duration": 1000,
+  "item_inputs": {
+      "item": "kubejs:hot_salt_crucible",
+      "amount": 1
+    },
+  "fluid_inputs": {
+      "fluid": "minecraft:water",
+      "amount": 1000
+    },
+  "item_outputs": [
+  {
+      "item": "modern_industrialization:salt_dust",
+      "amount": 3
+  },
+  {
+      "item": "kubejs:heatproof_crucible",
+      "amount": 1,
+	  "probability" : 0.95
   }
 ]
 })
